e184a009f29d8de8a37f7e77db40a8d69b33e867
Nightwind <terezsak@gmail.com>
2022-06-25 14:33:33 +0200

    User settings option #1
    User settings option #1

--- resources/js/Pages/Carrier/Settings.vue
+++ resources/js/Pages/Carrier/Settings.vue
@@ -1,25 +1,28 @@
 <template>
     <div class="py-12">
-    <div v-show="true" class="rounded-md flex items-center max-w-screen-xl mx-auto bg-amber-400 jusitfy-between px-5 py-4 mb-4 border border-yellow-500 text-white text-lg font-medium">
-        <div class="w-full flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class=" w-6 h-6 mr-2" viewBox="0 0 1792 1792">
-                <path d="M1024 1375v-190q0-14-9.5-23.5t-22.5-9.5h-192q-13 0-22.5 9.5t-9.5 23.5v190q0 14 9.5 23.5t22.5 9.5h192q13 0 22.5-9.5t9.5-23.5zm-2-374l18-459q0-12-10-19-13-11-24-11h-220q-11 0-24 11-10 7-10 21l17 457q0 10 10 16.5t24 6.5h185q14 0 23.5-6.5t10.5-16.5zm-14-934l768 1408q35 63-2 126-17 29-46.5 46t-63.5 17h-1536q-34 0-63.5-17t-46.5-46q-37-63-2-126l768-1408q17-31 47-49t65-18 65 18 47 49z">
-                </path>
-            </svg>
-            We use cookies! Something not ideal might be happening.
-        </div>
-        <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
-            <button type="button" class="-mr-1 flex p-2 rounded-md hover:bg-amber-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
-                <span class="sr-only">
-                    Dismiss
-                </span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="h-6 w-6 text-white" viewBox="0 0 1792 1792">
-                    <path d="M1490 1322q0 40-28 68l-136 136q-28 28-68 28t-68-28l-294-294-294 294q-28 28-68 28t-68-28l-136-136q-28-28-28-68t28-68l294-294-294-294q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 294 294-294q28-28 68-28t68 28l136 136q28 28 28 68t-28 68l-294 294 294 294q28 28 28 68z">
+    <div v-show="show_alert && !user_online" class="max-w-screen-xl mx-auto pb-6 px-4 sm:px-6 lg:px-8">
+        <div class="rounded-md flex items-center bg-amber-400 jusitfy-between px-5 py-4 border border-yellow-500 text-white text-lg font-medium">
+            <div class="w-full flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class=" w-6 h-6 mr-2" viewBox="0 0 1792 1792">
+                    <path d="M1024 1375v-190q0-14-9.5-23.5t-22.5-9.5h-192q-13 0-22.5 9.5t-9.5 23.5v190q0 14 9.5 23.5t22.5 9.5h192q13 0 22.5-9.5t9.5-23.5zm-2-374l18-459q0-12-10-19-13-11-24-11h-220q-11 0-24 11-10 7-10 21l17 457q0 10 10 16.5t24 6.5h185q14 0 23.5-6.5t10.5-16.5zm-14-934l768 1408q35 63-2 126-17 29-46.5 46t-63.5 17h-1536q-34 0-63.5-17t-46.5-46q-37-63-2-126l768-1408q17-31 47-49t65-18 65 18 47 49z">
                     </path>
                 </svg>
-            </button>
+                You are currently in offline mode, if you want orders to be assigned to you, please click the switch button below to go online.
+            </div>
+            <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+                <button @click="closeAlert()" type="button" class="-mr-1 flex p-2 rounded-md hover:bg-amber-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
+                    <span class="sr-only">
+                        Dismiss
+                    </span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="h-6 w-6 text-white" viewBox="0 0 1792 1792">
+                        <path d="M1490 1322q0 40-28 68l-136 136q-28 28-68 28t-68-28l-294-294-294 294q-28 28-68 28t-68-28l-136-136q-28-28-28-68t28-68l294-294-294-294q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 294 294-294q28-28 68-28t68 28l136 136q28 28 28 68t-28 68l-294 294 294 294q28 28 28 68z">
+                        </path>
+                    </svg>
+                </button>
+            </div>
         </div>
     </div>
+
     <main class="relative">
         <div class="max-w-screen-xl mx-auto pb-6 px-4 sm:px-6 lg:pb-16 lg:px-8">
             <div class="bg-white rounded-lg overflow-hidden">
@@ -164,6 +167,7 @@ export default {
         return {
             user_online: this.is_online,
             image_hover: false,
+            show_alert: true,
         };
     },
 
@@ -181,6 +185,9 @@ export default {
         hoverImage(parameter) {
             this.image_hover = parameter;
         },
+        closeAlert() {
+            this.show_alert = false;
+        }
 
     },
 };
